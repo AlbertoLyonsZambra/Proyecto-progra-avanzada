36737e9179a4d71e5ea5a2717f85289d2627aafe
AlbertoLyons <alberto.lyons@alumnos.ucn.cl>
2024-04-11 17:56:07 -0400

    test(movimiento): Movimiento por teclado

--- Assets/Scripts/Jugador.cs
+++ Assets/Scripts/Jugador.cs
@@ -4,33 +4,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private float velocidad = 0.01f;
-    //private Transform transform
-    // Start is called before the first frame update
+    private float velocidad = 5f;
     void Start()
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
-       
         //Movimiento a la izquierda
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
-                transform.Translate(-velocidad, 0, 0); 
+            Vector3 movimiento = new Vector3(-1, 0, 0) * velocidad * Time.deltaTime;
+            transform.Translate(movimiento);
         }
         //Movimiento a la derecha
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
-            transform.Translate(velocidad, 0, 0);
+            Vector3 movimiento = new Vector3(1, 0, 0) * velocidad * Time.deltaTime;
+            transform.Translate(movimiento);
         }
         //Movimiento hacia arriba
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) {
-                transform.Translate(0, velocidad, 0); 
+            Vector3 movimiento = new Vector3(0, 0, 1) * velocidad * Time.deltaTime;
+            transform.Translate(movimiento);
         }
         //Movimiento hacia abajo
         if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
-                transform.Translate(0, -velocidad, 0); 
+            Vector3 movimiento = new Vector3(0, 0, -1) * velocidad * Time.deltaTime;
+            transform.Translate(movimiento);
         }
     }
 }
