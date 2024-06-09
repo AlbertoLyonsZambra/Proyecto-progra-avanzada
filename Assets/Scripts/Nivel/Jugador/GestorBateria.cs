c63c1156b96318ff8d213a4c7ec2cae70bb62013
godchisa <Matias.254@outlook.com>
2024-06-09 17:54:29 -0400

    feat: funcionalidades individuales para las naves

--- Assets/Scripts/Nivel/Jugador/GestorBateria.cs
+++ Assets/Scripts/Nivel/Jugador/GestorBateria.cs
@@ -65,7 +65,7 @@ public class GestorBateria : MonoBehaviour
                 //Debug.Log("Bater�a agotada");
             }
 
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(1.0f);
         }
     }
 
