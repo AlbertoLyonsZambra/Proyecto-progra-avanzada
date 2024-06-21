174fa776319bcb5be86e05fd46e4048f758d4595
AlbertoLyons <alberto.lyons@alumnos.ucn.cl>
2024-06-21 01:38:05 -0400

    feat(Menu): Se agrega skybox en funcion del nivel

--- Assets/Scripts/GestorMenuNivel.cs
+++ Assets/Scripts/GestorMenuNivel.cs
@@ -9,26 +9,44 @@ public class GestorMenuNivel : MonoBehaviour
     [SerializeField] private AudioClip musicaNivel2;
     [SerializeField] private AudioClip musicaNivel3;
     [SerializeField] private AudioClip musicaNivel4;
+    [SerializeField] private Material skyboxNivel0;
+    [SerializeField] private Material skyboxNivel1;
+    [SerializeField] private Material skyboxNivel2;
+    [SerializeField] private Material skyboxNivel3;
+    [SerializeField] private Material skyboxNivel4;
     void Start()
     {
         AudioClip musicaPaPoner = musicaNivel0;
+        Material skyBoxPaPoner = skyboxNivel0;
         int nivel = PlayerPrefs.GetInt("nivelActual");
         if (nivel == 1)
         {
             musicaPaPoner = musicaNivel1;
+            skyBoxPaPoner = skyboxNivel1;
         }
         else if (nivel == 2)
         {
             musicaPaPoner = musicaNivel2;
+            skyBoxPaPoner = skyboxNivel2;
         }
         else if (nivel == 3)
         {
             musicaPaPoner = musicaNivel3;
+            skyBoxPaPoner = skyboxNivel3;
         }
         else if (nivel >= 4)
         {
             musicaPaPoner = musicaNivel4;
+            skyBoxPaPoner = skyboxNivel4;
         }
         Gestor_audio.Instance.EjecutarAudio(Gestor_audio.Instance.audioSourceMusica, musicaPaPoner);
+        if (skyBoxPaPoner != null)
+        {
+            RenderSettings.skybox = skyBoxPaPoner;
+        }
+        else
+        {
+            Debug.LogWarning("No hay skybox seleccionada");
+        }
     }
 }
